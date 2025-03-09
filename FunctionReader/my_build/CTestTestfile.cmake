8ac735fb399f5a0f5f919eda7f87dc0c3674eca2
Kingalldog <vsamohin@huskyeg>
2025-03-09 19:51:31 +0300

    Remove my-build

--- FunctionReader/my_build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vsamohin/Nelder-mead-optimization/FunctionReader
-# Build directory: /home/vsamohin/Nelder-mead-optimization/FunctionReader/my_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
