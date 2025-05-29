e83d2a47778bba42c8ed5e37b1221274041e017c
Roma Golubev <romag643@gmail.com>
2025-05-29 13:22:02 +0300

    Добавили отчёт

--- NelderMeadSolver/src/playground.cpp
+++ NelderMeadSolver/src/playground.cpp
@@ -1,36 +1,55 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <tuple>
 
 #include "solver.h"
 
+using Params = std::tuple<size_t, double, double, double, double, double, double>;
+using std::get;
+using std::to_string;
+
+Params ParseParams(const std::string& str) {
+    std::istringstream iss(str);
+    char sep;
+    Params params;
+    iss >> std::get<0>(params) >> std::get<1>(params) >> std::get<2>(params) >>
+        std::get<3>(params) >> std::get<4>(params) >> sep >> std::get<5>(params) >> sep >>
+        std::get<6>(params);
+    return params;
+};
+
 int main(int argc, char* argv[]) {
-    SLV::LoggerPtr logger = SLV::Logger::GetLogger();
-    SLV::NelderMeadSolver solver(logger);
-    SLV::OptInfo params;
-    std::cout << "Введите функцию: ";
-    std::getline(std::cin, params.function);
-    size_t cnt_dim = 0;
-    try {
-        cnt_dim = solver.CountDim(params.function);
-    } catch (std::runtime_error& ex) {
-        std::cerr << "Произошла ошибка: " << ex.what();
-        return 1;
+    std::string filename;
+    std::cin >> filename;
+    std::ifstream input(filename);
+    SLV::LoggerPtr logger{SLV::Logger::GetLogger()};
+
+    while (!input.eof()) {
+        std::string func, cnt_launchs;
+        std::getline(input, func);
+        std::cout << func;
+        std::getline(input, cnt_launchs);
+        std::cout << cnt_launchs;
+        size_t cnt = std::stoul(cnt_launchs);
+        for (size_t i = 0; i < cnt && !input.eof(); ++i) {
+            std::string settings;
+            std::getline(input, settings);
+            Params params = ParseParams(settings);
+            Point p{{get<5>(params), get<6>(params)}};
+            SLV::NelderMeadSolver solver(logger, get<4>(params), get<2>(params), get<3>(params));
+            solver.Optimize({func, get<0>(params), get<1>(params), p});
+            std::string new_file = filename + "/" + func + "_" + to_string(get<0>(params)) + "_" +
+                                   to_string(get<1>(params)) + "_" + to_string(get<2>(params)) +
+                                   "_" + to_string(get<3>(params)) + "_" +
+                                   to_string(get<4>(params)) + "_" + to_string(get<5>(params)) +
+                                   "_" + to_string(get<6>(params)) + ".txt";
+            std::ofstream out(new_file);
+            auto logs = logger->GetLogs(&solver);
+            for (const auto& log : logs) {
+                out << log.func_val << " " << log.measure << std::endl;
+            }
+            out.close();
+        }
     }
-
-    std::cout << "Введите стартовую точку. Размерность: " << cnt_dim << std::endl;
-    std::vector<double> coords(cnt_dim, 0);
-    for (size_t i = 0; i < cnt_dim; ++i) {
-        std::cout << "#1 - ";
-        std::cin >> coords[i];
-    }
-    params.start_point = Point(coords);
-
-    std::cout << "Введите число итераций: ";
-    std::cin >> params.epoch;
-
-    std::cout << "Введите мин меру симплекса: ";
-    std::cin >> params.measure;
-
-    std::cout << "min Q(X) = " << solver.Optimize(params);
-    logger->WriteHTML(&solver);
-    return 0;
 }
